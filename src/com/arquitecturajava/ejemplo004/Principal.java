aeaea1ea3644bd8fdfb9bac780eff15bd608a0ae
Harold Portocarrero <hportocarrero@todo1.com>
2017-11-14 13:45:07 -0500

    ejemplo usando procedimientos almacenados

---
+++ src/com/arquitecturajava/ejemplo004/Principal.java
@@ -0,0 +1,28 @@
+package com.arquitecturajava.ejemplo004;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		String cadenaConexion = "jdbc:mysql://localhost:3306/curso";
+
+		try (Connection conexion = DriverManager.getConnection(cadenaConexion, "root", "123");
+				CallableStatement sentencia = conexion.prepareCall("{CALL seleccionarPersonas()}");
+				ResultSet rs = sentencia.executeQuery();
+				) {
+			while(rs.next()) {
+				System.out.println(rs.getString("nombre"));
+				System.out.println(rs.getString("apellidos"));
+				System.out.println(rs.getInt("edad"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
