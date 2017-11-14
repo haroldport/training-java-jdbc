68cb5f5a9dbacfb07a80915bf1d91289ce99bfb6
Harold Portocarrero <hportocarrero@todo1.com>
2017-11-14 09:43:04 -0500

    cerrando recursos con clausula finally

--- src/com/arquitecturajava/ejemplo001/Principal.java
+++ src/com/arquitecturajava/ejemplo001/Principal.java
@@ -9,19 +9,29 @@ public class Principal {
 
 	public static void main(String[] args) {
 		
+		Connection conexion = null;
+		Statement sentencia = null;
+		
 		try {
 			//registra el driver de mysql
 			//podriamos tener mas drivers instalados
 			Class.forName("com.mysql.jdbc.Driver");
 			String cadenaConexion = "jdbc:mysql://localhost:3306/curso";
-			Connection conexion = DriverManager.getConnection(cadenaConexion, "root", "123");
-			Statement sentencia = conexion.createStatement();
+			conexion = DriverManager.getConnection(cadenaConexion, "root", "123");
+			sentencia = conexion.createStatement();
 			sentencia.executeUpdate("insert into persona (nombre, apellidos, edad) "
 					+ "values ('Harold', 'Portocarrero', 25)");
-			sentencia.close();
-			conexion.close();
 		} catch (ClassNotFoundException | SQLException e) {
 			e.printStackTrace();
+		} finally {
+			try {
+				if(sentencia != null) {
+					sentencia.close();
+				}
+				conexion.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
 		}
 
 	}
